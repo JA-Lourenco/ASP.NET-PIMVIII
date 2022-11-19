75d8f11dcd07af4ff7b27a770ee56c3dd0d6fdda
JA-Lourenco <ja.lourenco@outlook.com>
2022-11-19 18:06:07 -0300

    Pagina de agendamento realizado

---
+++ Pages/AgRealizado.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PIMVIII.Pages
+{
+    public class AgRealizadoModel : PageModel
+    {
+        private readonly ILogger<AgRealizadoModel> _logger;
+
+        public AgRealizadoModel(ILogger<AgRealizadoModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
