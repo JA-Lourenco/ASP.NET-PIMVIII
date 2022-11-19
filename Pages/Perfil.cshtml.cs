89b9d5b3c7b8251e69253b9d4b3cd690656c0155
JA-Lourenco <ja.lourenco@outlook.com>
2022-11-19 18:29:18 -0300

    Pagina de perfil

---
+++ Pages/Perfil.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PIMVIII.Pages
+{
+    public class PerfilModel : PageModel
+    {
+        private readonly ILogger<PerfilModel> _logger;
+
+        public PerfilModel(ILogger<PerfilModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
