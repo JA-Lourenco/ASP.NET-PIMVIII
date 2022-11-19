23eeeaf6d9ffcd623b3721e8b35724bcc0e25b0e
JA-Lourenco <ja.lourenco@outlook.com>
2022-11-19 19:37:39 -0300

    Pagina de dados pessoais

---
+++ Pages/DadosPessoais.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PIMVIII.Pages
+{
+    public class DadosPessoaisModel : PageModel
+    {
+        private readonly ILogger<DadosPessoaisModel> _logger;
+
+        public DadosPessoaisModel(ILogger<DadosPessoaisModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
