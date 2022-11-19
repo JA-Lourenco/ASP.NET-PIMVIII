f8e735e3c9f51e209bba42f7e535f0909bccad21
JA-Lourenco <ja.lourenco@outlook.com>
2022-11-19 17:07:37 -0300

    Pagina de agendamento

---
+++ Pages/Agendamento.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PIMVIII.Pages
+{
+    public class AgendamentoModel : PageModel
+    {
+        private readonly ILogger<AgendamentoModel> _logger;
+
+        public AgendamentoModel(ILogger<AgendamentoModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
