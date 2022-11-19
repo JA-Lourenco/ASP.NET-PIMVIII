a696ab1042665b1b8b760a40974869c72a9fc53a
JA-Lourenco <ja.lourenco@outlook.com>
2022-11-19 12:14:21 -0300

    first commit

---
+++ Pages/Home.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PIMVIII.Pages
+{
+    public class HomeModel : PageModel
+    {
+        private readonly ILogger<HomeModel> _logger;
+
+        public HomeModel(ILogger<HomeModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
