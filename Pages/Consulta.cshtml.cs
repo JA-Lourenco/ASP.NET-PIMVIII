be4d54a71941e7fa4f79ec63526c7ed704289388
JA-Lourenco <ja.lourenco@outlook.com>
2022-11-19 21:39:49 -0300

    Pagina de consulta

---
+++ Pages/Consulta.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PIMVIII.Pages
+{
+    public class ConsultaModel : PageModel
+    {
+        private readonly ILogger<ConsultaModel> _logger;
+
+        public ConsultaModel(ILogger<ConsultaModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
